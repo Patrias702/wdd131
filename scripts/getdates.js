9d190b915238baf5477f4b6b7e98dbe4d3b299a8
DJ Patrias <Patrias21@gmail.com>
2024-09-21 17:17:23 -0700

    Done

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const yearSpan = document.getElementById('year');
+    const lastModifiedSpan = document.getElementById('last-modified');
+
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+
+    yearSpan.textContent = currentYear;
+    lastModifiedSpan.textContent = lastModified;
+});
+
